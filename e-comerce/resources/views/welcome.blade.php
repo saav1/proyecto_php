8d1f7c98b5537c7464abdebfdd199d2b66644c21
saav1 <saav1@alu.ua.es>
2020-04-13 17:31:05 +0200

    e-comerce layout css grid

--- e-comerce/resources/views/welcome.blade.php
+++ e-comerce/resources/views/welcome.blade.php
@@ -1,100 +1,134 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{app()->getLocale()}}">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>CSS Grid Example</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat|Roboto:300,400,700">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" class="">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <header>
+            <div class="top-nav container">
+                <div class="logo">CSS Grid Example</div>
+                <ul>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Cart</a></li>
+                </ul>
+            </div> <!-- end top nav -->
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+            <div class="hero container">
+                <div class="hero-copy">
+                    <h1>CSS Grid Example</h1>
+                    <p>A practical example of using CSS Grid for a typical website layout.</p>
+                    <div class="hero-buttons">
+                        <a href="" class="button button-white">Button 1</a>
+                        <a href="" class="button button-white">Button 2</a>
+                    </div>
+                </div> <!-- end hero copy -->
+                
+                <div class="hero-image">
+                    <img src="img/macbook-pro-laravel.png" alt="hero image">
                 </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            </div> <!-- end hero -->
+        </header>
+        
+        <div class="featured-section">
+            <div class="container">
+                <h1 class="text-center">CSS Grid Example</h1>
+                <p class="section-description"> Description about the featured section</p>
+                
+                <div class="text-center button-container">
+                    <a href="#" class="button">Featured</a>
+                    <a href="#" class="button">On Sale</a>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="products text-center"> <!-- To start it will be only 3 products in this section -->
+                    <div class="product">
+                        <a href="" class=""><img src="img/macbook-pro.png" alt="product"></a>
+                        <a href="" class=""><div class="product-name">Mackbook Pro</div></a>
+                        <div class="product-price">$2499.99</div>
+                    </div>
+
+                    <div class="product">
+                        <a href="" class=""><img src="img/macbook-pro.png" alt="product"></a>
+                        <a href="" class=""><div class="product-name">Mackbook Pro</div></a>
+                        <div class="product-price">$2499.99</div>
+                    </div>
+
+                    <div class="product">
+                        <a href="" class=""><img src="img/macbook-pro.png" alt="product"></a>
+                        <a href="" class=""><div class="product-name">Mackbook Pro</div></a>
+                        <div class="product-price">$2499.99</div>
+                    </div>
+
+                </div> <!-- end products -->
+
+                <div class="text-center button-container">
+                    <a href="#" class="button">View more products</a>
+                
                 </div>
+
+            
+            </div> <!-- end container -->
+
+        </div> <!-- end featured section -->
+
+        <div class="blog-section">    
+            <div class="container">
+            
+                <h1 class="text-center">Fromo Our Blog</h1>
+                <p class="section-description"> Description from our blog. </p>
+
+                <div class="blog-posts">
+                    <div class="blog-post">
+                        <a href="#"><img src="img/blog1.png" alt="blog image"></a>
+                        <a href="#"><h2 class="blog-title">Blog Post Title 1</h2></a>
+                        <div class="blog-description">description blog post 1</div>
+                    </div>
+
+                    <div class="blog-post">
+                        <a href="#"><img src="img/blog2.png" alt="blog image"></a>
+                        <a href="#"><h2 class="blog-title">Blog Post Title 2</h2></a>
+                        <div class="blog-description">description blog post 2</div>
+                    </div>
+
+                    <div class="blog-post">
+                        <a href="#"><img src="img/blog3.png" alt="blog image"></a>
+                        <a href="#"><h2 class="blog-title">Blog Post Title 3</h2></a>
+                        <div class="blog-description">description blog post 3</div>
+                    </div>
+                </div> <!-- end blog-posts-->
+            </div> <!-- end container -->
+        </div> <!-- end blog section -->
+
+        <footer>
+            <div class="footer-content container">
+                <div class="made-with">Made by Stalyn Alejandro</div>
+                <ul>
+                    <il>Follow me:</il>
+                    <il><a href=""><i class="fa fa-globe"></i></a></il>
+                    <il><a href=""><i class="fa fa-youtube"></i></a></il>
+                    <il><a href=""><i class="fa fa-github"></i></a></il>
+                    <il><a href=""><i class="fa fa-twitter"></i></a></il>
+                </ul>
             </div>
-        </div>
+        
+        </footer>
+
     </body>
 </html>
+
+
