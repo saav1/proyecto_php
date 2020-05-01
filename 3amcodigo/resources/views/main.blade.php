c67099ce26c32f2bab6335e4e5265f782960cccd
saav1 <saav1@alu.ua.es>
2020-05-01 13:45:24 +0200

    s

--- 3amcodigo/resources/views/main.blade.php
+++ 3amcodigo/resources/views/main.blade.php
@@ -23,7 +23,7 @@
                     <li ><a href="#" class="">Algoritmos</a></li>
                     <li ><a href="#" class="">C++</a></li>
                     <li ><a href="#" class="">Java</a></li>
-                    <li ><a href="#" class="">Info.</a></li>
+                    <li ><a href="#" class="">Log in</a></li>
                 </ul>
             </div> <!-- end top nav-->
 
@@ -38,11 +38,22 @@
 
         </header>
 
+
+        <footer>
+            <div class="footer-content container">
+                <div class="made-with">Made by saav</div>
+                <ul>
+                    <il></il>
+                    <il><a href=""><i class="fa fa-globe"></i></a></il>
+                        <il><a href=""><i class="fa fa-youtube"></i></a></il>
+                        <il><a href=""><i class="fa fa-github"></i></a></il>
+                        <il><a href=""><i class="fa fa-twitter"></i></a></il>
+                </ul>
+            </div>
+        </footer>
+
     </body>
 
-    <footer>
-    
-    </footer>
 
 
 </html>
